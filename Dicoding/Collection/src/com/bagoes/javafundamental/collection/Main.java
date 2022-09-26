688a997914c0660cced3c24c7d8b8e4e38e3de80
bagoes <bagoessoemargo@gmail.com>
2022-09-26 18:51:20 +0700

    Belajar Collection Java

---
+++ Dicoding/Collection/src/com/bagoes/javafundamental/collection/Main.java
@@ -0,0 +1,9 @@
+package com.bagoes.javafundamental.collection;
+
+import java.util.Collection;
+
+public class Main {
+    public static void main(String[] args) {
+        Collection collection;
+    }
+}
