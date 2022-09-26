978ca480c830edeff7082ebe23d2ecdd7464eddc
bagoes <bagoessoemargo@gmail.com>
2022-09-26 18:40:48 +0700

    Belajar Operator Java

---
+++ Dicoding/Operator/src/com/bagoes/javafundamental/operator/OperatorAssignment.java
@@ -0,0 +1,11 @@
+package com.bagoes.javafundamental.operator;
+
+public class OperatorAssignment {
+
+    public static void main(String[] args) {
+        int value = 2;
+        int anotherValue = 19;
+        System.out.println("Data pada value adalah ->> " + value);
+        System.out.println("Data pada another value adalah ->> " + anotherValue);
+    }
+}
