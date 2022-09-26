61bfb51a9175aec461e5b928341533eccbb3ad5c
bagoes <bagoessoemargo@gmail.com>
2022-09-27 00:38:16 +0700

    Belajar Interface Implement Java

---
+++ Dicoding/InterfaceImplement/src/com/bagoes/javafundamental/oop/Hewan.java
@@ -0,0 +1,7 @@
+package com.bagoes.javafundamental.oop;
+
+public interface Hewan {
+    String respirasi = "oksigen";
+
+    void makan();
+}
---
+++ Dicoding/InterfaceImplement/src/com/bagoes/javafundamental/oop/Kucing.java
@@ -0,0 +1,8 @@
+package com.bagoes.javafundamental.oop;
+
+public  class Kucing implements Hewan {
+    @Override
+    public void makan() {
+
+    }
+}
